2244965fb3b6c281412faa481cbd8dea54c2be7a
GSC Bravo <gscbravo@gmail.com>
2023-06-20 23:26:04 -0500

    go across pages

--- src/scred.py
+++ src/scred.py
@@ -1,13 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_subreddit(subreddit, session = None):
+# User-Agent set to Chrome 114 on Windows 10
+headers = {
+    'User-Agent': 'Mozilla/5.0 (compatible; scred/1.0; +https://github.com/gscbravo/scred)'
+}
+
+def get_subreddit(subreddit, pages = 1, session = None):
     '''
     Returns a list of first page posts on subreddit.
 
     Args:
         subreddit:
             subreddit name, without 'r/'
+        pages = 1:
+            number of pages to retrieve, default is 1
         session = None:
             'reddit_session' cookie, used for private subreddits
 
@@ -17,6 +24,8 @@ def get_subreddit(subreddit, session = None):
                 poster username, without 'u/'
             comments:
                 link to the post comments
+            id:
+                post short id
             link:
                 link given by the post, if none is given, same as 'comments'
             timestamp:
@@ -28,10 +37,6 @@ def get_subreddit(subreddit, session = None):
                 number of votes the post has
     '''
 
-    # User-Agent set to Chrome 114 on Windows 10
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (compatible; scred/1.0; +https://github.com/gscbravo/scred)'
-    }
     # cookies to allow viewing gated and NSFW subreddit
     cookies = {
         '_options': '%7B%22pref_gated_sr_optin%22%3A%20true%7D',
@@ -40,45 +45,60 @@ def get_subreddit(subreddit, session = None):
     }
 
     url = f"https://old.reddit.com/r/{subreddit}/"
-    response = requests.get(url, headers=headers, cookies=cookies)
-    soup = BeautifulSoup(response.content, 'lxml')
-
     posts = []
-    for entry in soup.select('div.link:not(.promoted)'):
-        # get entry author, setting to [deleted] if not present
-        author_link = entry.select_one('a.author')
-        if author_link is not None:
-            entry_author = author_link.text
-        else:
-            entry_author = '[deleted]'
 
-        # get entry title
-        entry_title = entry.select_one('a.title').text
+    # go through the pages
+    for i in range(pages):
+        response = requests.get(url, headers=headers, cookies=cookies)
+        soup = BeautifulSoup(response.content, 'lxml')
 
-        # get entry link, determining whether relative link or not
-        entry_href = entry.select_one('a.title')['href']
-        if not entry_href.startswith('/'):
-            entry_link = entry_href
-        else:
-            entry_link = f"https://old.reddit.com{entry_href}"
+        # get the posts, exclude ads
+        for entry in soup.select('div.link:not(.promoted)'):
+            # get entry author, setting to [deleted] if not present
+            author_link = entry.select_one('a.author')
+            if author_link is not None:
+                entry_author = author_link.text
+            else:
+                entry_author = '[deleted]'
 
-        # get commnts
-        entry_comments = entry.select_one('a.comments')['href']
+            # get entry title
+            entry_title = entry.select_one('a.title').text
 
-        # get votes
-        entry_votes = entry.select_one('div.score').text
+            # get entry link, determining whether relative link or not
+            entry_href = entry.select_one('a.title')['href']
+            if not entry_href.startswith('/'):
+                entry_link = entry_href
+            else:
+                entry_link = f"https://old.reddit.com{entry_href}"
 
-        # get timestamp
-        entry_timestamp = entry.select_one('time.live-timestamp')['datetime']
+            # get commnts
+            entry_comments = entry.select_one('a.comments')['href']
 
-        # add the info
-        posts.append({
-            'author': entry_author,
-            'comments': entry_comments,
-            'link': entry_link,
-            'timestamp': entry_timestamp,
-            'title': entry_title,
-            'votes': entry_votes
-        })
+            # get votes
+            entry_votes = entry.select_one('div.score').text
+
+            # get timestamp
+            entry_timestamp = entry.select_one('time.live-timestamp')['datetime']
+
+            # get id
+            entry_id = entry_comments.split('/')[6]
+
+            # add the info
+            posts.append({
+                'author': entry_author,
+                'comments': entry_comments,
+                'id': entry_id,
+                'link': entry_link,
+                'timestamp': entry_timestamp,
+                'title': entry_title,
+                'votes': entry_votes
+            })
+
+        # get the next button and set url to it, escape if it doesn't exist
+        next_page = soup.select_one('span.next-button a')
+        if next_page is None:
+            return posts
+        
+        url = next_page['href']
     
     return posts
